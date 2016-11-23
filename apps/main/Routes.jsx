e7e74c7a9616045be1122620508143d8202bffcc
Vladimir Makhaev <vmakhaev@gmail.com>
2016-11-23 22:13:38 +0300

    fix for case issue

--- apps/main/Routes.jsx
+++ apps/main/Routes.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import Root from './pages/Root'
 import CreatePage from './pages/CreatePage'
-import itemPage from './pages/itemPage'
+import ItemPage from './pages/ItemPage'
 import ListPage from './pages/ListPage'
 
 let Routes = (
   <Route path='/' component={Root}>
     <IndexRoute component={ListPage} />
     <Route path='create' component={CreatePage} />
-    <Route path='items/:itemId' component={itemPage} />
+    <Route path='items/:itemId' component={ItemPage} />
     <Route path='list' component={ListPage} />
   </Route>
 )
